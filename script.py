640bf1db1db14165adcccb6e26fe201be95bb2c9
Nicolás Quintana <nicolas.quintana@usach.cl>
2020-06-07 21:07:44 -0400

    Se crea función que mediante el parámetro rutaHistorico (ruta del archivo csv) y otro parámetro numero que es el número de la licitación (por orden, para obtener el primero, segundo, tercero, etc.) se obtenga el json limpio

--- script.py
+++ script.py
@@ -29,59 +29,58 @@ def reemplazoAux(entrada):
     entrada = entrada.replace("\"       \"","\",       \"")
     return entrada
 
-size = 1
-df = pd.read_csv("c:/Personal/historicoJsonLicitaciones.csv", chunksize=size)
+def obtenerJson(rutaHistorico, numero):
+    size = 1
+    df = pd.read_csv(rutaHistorico, chunksize=size)
+    i = 0
+    for linea in df:
+        dataframe1 = linea
+        if(i == numero):
+            break
+        i = i + 1
+    # En la variable dataframe1 queda el primer dataframe
+    dfString = dataframe1.to_string()
+    #print("paso 1:\n")
+    #print(dfString)
+    # Ahora en dfString queda el primer dataframe como string
 
-i = 0
-for linea in df:
-    dataframe1 = linea
-    i = i + 1
-    if(i == 1):
-        break
+    listadoLineasPrimerDF = dfString.split('\n')
+    #print("paso 2:\n")
+    #print(listadoLineasPrimerDF)
 
+    # Ahora se tiene un listado de dos líneas en listadoLineasPrimerDF
+    # listadoLineasPrimerDF[0] = id;detalleJson;link
+    # listadoLineasPrimerDF[1] = idLicitacion1;detalleJson1;linkLicitacion1
+    listadoColumnasPrimerDF = listadoLineasPrimerDF[1].strip().split(';')
+    # Ahora se debería tener en listadoColumnasPrimerDF
+    # listadoColumnasPrimerDF[0] = ID de la licitación 1
+    # listadoColumnasPrimerDF[1] = Detalle Json Licitación 1
+    # listadoColumnasPrimerDF[2] = Link de la licitación
+    #print("ID DE LICITACIÓN 1")
+    #print(listadoColumnasPrimerDF[0].strip())
+    #print("JSON DE LICITACIÓN 1")
+    #print(listadoColumnasPrimerDF[1].strip())
+    #print("LINK DE LICITACIÓN 1")
+    #print(listadoColumnasPrimerDF[2].strip())
 
+    #Nos concentraremos en print(listadoColumnasPrimerDF[1].strip()) que tiene el json de la licitación
+    # Como el detalle del json está entre comillas, se sacará el primer y último caracter
+    jsonPelado = listadoColumnasPrimerDF[1].strip()[1:][:-1].strip()
+    # También se reemplazará las dobledoble comilla por doble comilla    Ejemplo ""Nombre"": ""Nicolas Quintana"" ---> "Nombre": "Nicolas Quintana"
+    jsonPelado = jsonPelado.replace("\"\"","\"")
+    # Parte del json está de la siguiente manera:
+    # "Obras": "0"  "Estado": "Adjudicada"  "Etapas": 1
+    # --> lo cual debería estar así
+    # "Obras": "0", "Estado": "Adjudicada", "Etapas": 1
+    # Por lo tanto, se reemplazará '"  "' por '", "'
+    jsonPelado = jsonPelado.replace("\"  \"", "\", \"")
+    # Ahora está el inconveniente, que aveces hay en vez de que haya:
+    # "Obras": "0"  "Estado": "Adjudicada"  "Etapas": 1
+    # Hay:
+    # "Obras": 0  "Estado": "Adjudicada"  "Etapas": 1
+    # Por lo tanto, ya no sirve el reemplazar "  " por ", "
+    # Se hará un método para reemplazar un número (del 0 al 9) que después tenga dos espacios y una comilla doble, por el número + "," + " " + comilla doble
+    jsonPelado = reemplazoAux(jsonPelado)
+    return jsonPelado
 
-# En la variable dataframe1 queda el primer dataframe
-dfString = dataframe1.to_string()
-#print("paso 1:\n")
-#print(dfString)
-# Ahora en dfString queda el primer dataframe como string
-
-listadoLineasPrimerDF = dfString.split('\n')
-#print("paso 2:\n")
-#print(listadoLineasPrimerDF)
-
-# Ahora se tiene un listado de dos líneas en listadoLineasPrimerDF
-# listadoLineasPrimerDF[0] = id;detalleJson;link
-# listadoLineasPrimerDF[1] = idLicitacion1;detalleJson1;linkLicitacion1
-listadoColumnasPrimerDF = listadoLineasPrimerDF[1].strip().split(';')
-# Ahora se debería tener en listadoColumnasPrimerDF
-# listadoColumnasPrimerDF[0] = ID de la licitación 1
-# listadoColumnasPrimerDF[1] = Detalle Json Licitación 1
-# listadoColumnasPrimerDF[2] = Link de la licitación
-#print("ID DE LICITACIÓN 1")
-#print(listadoColumnasPrimerDF[0].strip())
-#print("JSON DE LICITACIÓN 1")
-#print(listadoColumnasPrimerDF[1].strip())
-#print("LINK DE LICITACIÓN 1")
-#print(listadoColumnasPrimerDF[2].strip())
-
-#Nos concentraremos en print(listadoColumnasPrimerDF[1].strip()) que tiene el json de la licitación
-# Como el detalle del json está entre comillas, se sacará el primer y último caracter
-jsonPelado = listadoColumnasPrimerDF[1].strip()[1:][:-1].strip()
-# También se reemplazará las dobledoble comilla por doble comilla    Ejemplo ""Nombre"": ""Nicolas Quintana"" ---> "Nombre": "Nicolas Quintana"
-jsonPelado = jsonPelado.replace("\"\"","\"")
-# Parte del json está de la siguiente manera:
-# "Obras": "0"  "Estado": "Adjudicada"  "Etapas": 1
-# --> lo cual debería estar así
-# "Obras": "0", "Estado": "Adjudicada", "Etapas": 1
-# Por lo tanto, se reemplazará '"  "' por '", "'
-jsonPelado = jsonPelado.replace("\"  \"", "\", \"")
-# Ahora está el inconveniente, que aveces hay en vez de que haya:
-# "Obras": "0"  "Estado": "Adjudicada"  "Etapas": 1
-# Hay:
-# "Obras": 0  "Estado": "Adjudicada"  "Etapas": 1
-# Por lo tanto, ya no sirve el reemplazar "  " por ", "
-# Se hará un método para reemplazar un número (del 0 al 9) que después tenga dos espacios y una comilla doble, por el número + "," + " " + comilla doble
-jsonPelado = reemplazoAux(jsonPelado)
-print(jsonPelado)
+print(obtenerJson("c:/personal/historicoJsonLicitaciones.csv", 5))
